ae60dde0d9f1a83b3e7475f0294c7a28fcdc97b5
nknk710 <e1697@icloud.com>
2020-06-01 22:53:00 +0000

    a

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,11 +10,11 @@ Qyou(エンジニアのためのQ＆Aサイト)
 
 
 @section('content')
-
+    <div class="top-image">
+        <img class="back-image" src="{{ secure_asset('image/programming-1873854_1280.webp') }}" alt="">
+    </div>
     <div class="container">
-        <div class="top-image">
-            <img class="back-image" src="{{ secure_asset('image/programming-1873854_1280.webp') }}" alt="">
-        </div>
+        
         <div class="left-contents">
             <div class="main-contents-left">
                 <div class="question-contents">
