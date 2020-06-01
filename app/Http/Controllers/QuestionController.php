40285d01e73b3b59988bd3c9a55803bf754cfab8
nknk710 <e1697@icloud.com>
2020-06-01 23:38:25 +0000

    a

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -92,7 +92,7 @@ class QuestionController extends Controller
     public function show(Request $request){
         $question = Question::find($request->id);
         $answers = Answer::orderBy('created_at','desc')->where('question_id', $request->id)->get();
-        if(Auth::id() !== null){
+        if(Auth::user()){
             $bookmark = $question->bookmarks->where('user_id', Auth::user()->id)->first();
         }
         return view('questions.question',['question' => $question, 'answers' => $answers, 'bookmark'=>$bookmark]);
